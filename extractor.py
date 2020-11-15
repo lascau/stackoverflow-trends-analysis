739837d90c0e5a48a344fc8fd9eed983d5c5388c
Lascau Ionut Sebastian <lascauionutsebastian98@gmail.com>
2020-11-16 00:41:13 +0200

    refactored

--- extractor.py
+++ extractor.py
@@ -7,48 +7,64 @@ import pandas as pd
 import os
 from json_debugger import JsonDebugger
 
-key = 'AzNRstImwHbaQF1FLdR6DQ(('
+class Extractor:
 
-question_title = []
-question_content = []
-question_creation_date = []
+    def __init__(self, columns_names_list, json_keys_list):
+        self.key = 'AzNRstImwHbaQF1FLdR6DQ(('
+        self.columns_names_list = columns_names_list
+        self.json_keys_list = json_keys_list
+        self.columns_list = []
+        for i in range(len(columns_names_list)):
+            self.columns_list.append([])
+        
 
-for current_page in range(1):
-    url = 'https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&pagesize=100&filter=!9_bDDx5MI&page='+ str(current_page + 1)
-    headers = {"Accept": "application/json"}
-    auth = HTTPBasicAuth('apikey', 'AzNRstImwHbaQF1FLdR6DQ((')
-    response = requests.get(url, headers = headers, auth = auth)
+    def extract_from(self, endpoint):
+        for current_page in range(1):
+            current_url = endpoint + str(current_page + 1)
+            headers = {"Accept": "application/json"}
+            auth = HTTPBasicAuth('apikey', self.key)
+            response = requests.get(current_url, headers = headers, auth = auth)
     
-    if response.status_code == requests.codes.ok:
-        newData = json.loads(response.text)
-        debugger = JsonDebugger(response)
-        debugger.debug()
-        for item in newData['items']:
-            question_title.append(item['title'])
-            question_content.append(item['body_markdown'])
-            question_creation_date.append(datetime.fromtimestamp(item['creation_date']))
-        print("Processed page " + str(current_page + 1) + ", returned " + str(response))
-        # timeout not to be rate-limited
-        time.sleep(2)
-    else:
-        print('Error:', response.status_code, sep = ' ')
+            if response.status_code == requests.codes.ok:
+                newData = json.loads(response.text)
+                debugger = JsonDebugger(response)
+                debugger.debug()
+                for item in newData['items']:
+                    for json_key in self.json_keys_list:
+                        index = self.json_keys_list.index(json_key)
+                        if 'date' in json_key:
+                            self.columns_list[index].append(datetime.fromtimestamp(item[json_key]))
+                        else:               
+                            self.columns_list[index].append(item[json_key])
+                print("Processed page " + str(current_page + 1) + ", returned " + str(response))
+                # timeout not to be rate-limited
+                time.sleep(2)
+            else:
+                print('Error:', response.status_code, sep = ' ')
 
-current_directory = os.getcwd()
-extracted_data_directory = os.path.join(current_directory, r'extracted_data')
-if not os.path.exists(extracted_data_directory):
-    os.makedirs(extracted_data_directory)
+    def export_to_csv(self, csv_name):
+        current_directory = os.getcwd()
+        extracted_data_directory = os.path.join(current_directory, r'extracted_data')
+        if not os.path.exists(extracted_data_directory):
+            os.makedirs(extracted_data_directory)
 
 
-questions_dataframe = pd.DataFrame(list(zip(question_title, question_content, question_creation_date)), 
-                         columns = ['Title', 'Content', 'Creation Date'])
-questions_dataframe.to_csv('extracted_data/questions.csv')
+        data = pd.DataFrame(zip(*self.columns_list), 
+                         columns = self.columns_names_list)
+        data.to_csv('extracted_data/' + csv_name + '.csv')
 
-'''
-#Debug
-for title, content in zip(question_title, question_content):
-    print(f'Title is: {title}\nContent is: {content}')
-for d in question_creation_date:
-    print(f'My date: {d}')
-'''
 
+if __name__ == '__main__':
+    
+    questions_extractor = Extractor(['Title', 'Content', 'Creation Date'], ['title', 'body_markdown', 'creation_date'])
+    questions_extractor.extract_from('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&pagesize=100&filter=!9_bDDx5MI&page=')
+    questions_extractor.export_to_csv('questions')
+
+    answers_extractor = Extractor(['Answer', 'Creation Date'], ['body_markdown', 'creation_date'])
+    answers_extractor.extract_from('https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow&pagesize=100&filter=!9_bDE(S6I&page=')
+    answers_extractor.export_to_csv('answers')
+
+    comments_extractor = Extractor(['Comment', 'Creation Date'], ['body', 'creation_date'])
+    comments_extractor.extract_from('https://api.stackexchange.com/2.2/comments?order=desc&sort=creation&site=stackoverflow&pagesize=100&filter=!9_bDE0E4s&page=')
+    comments_extractor.export_to_csv('comments')
 
