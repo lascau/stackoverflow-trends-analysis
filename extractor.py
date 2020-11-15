37067059e0a93f88f1cb34a55549b2887795d5cb
Lascau Ionut Sebastian <lascauionutsebastian98@gmail.com>
2020-11-15 16:31:24 +0200

    exported data to csv

--- extractor.py
+++ extractor.py
@@ -2,12 +2,17 @@ import requests # for doing http requests to a resource
 import json # to serialize/deserialize the json getting from the call
 import time # for delay with 2 s each api request
 from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import pandas as pd
+import os
 
 
 key = 'AzNRstImwHbaQF1FLdR6DQ(('
 
 question_title = []
-questions_content = []
+question_content = []
+question_creation_date = []
+
 for current_page in range(1):
     url = 'https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&pagesize=100&filter=!9_bDDx5MI&page='+ str(current_page + 1)
     headers = {"Accept": "application/json"}
@@ -31,15 +36,30 @@ for current_page in range(1):
 
         for item in newData['items']:
             question_title.append(item['title'])
-            questions_content.append(item['body_markdown'])
+            question_content.append(item['body_markdown'])
+            question_creation_date.append(datetime.fromtimestamp(item['creation_date']))
         print("Processed page " + str(current_page + 1) + ", returned " + str(response))
         # timeout not to be rate-limited
         time.sleep(2)
     else:
         print('Error:', response.status_code, sep = ' ')
 
+current_directory = os.getcwd()
+extracted_data_directory = os.path.join(current_directory, r'extracted_data')
+if not os.path.exists(extracted_data_directory):
+    os.makedirs(extracted_data_directory)
+
+
+questions_dataframe = pd.DataFrame(list(zip(question_title, question_content, question_creation_date)), 
+                         columns = ['Title', 'Content', 'Creation Date'])
+questions_dataframe.to_csv('extracted_data/questions.csv')
+
 '''
 #Debug
-for title, content in zip(question_title, questions_content):
+for title, content in zip(question_title, question_content):
     print(f'Title is: {title}\nContent is: {content}')
+for d in question_creation_date:
+    print(f'My date: {d}')
 '''
+
+
