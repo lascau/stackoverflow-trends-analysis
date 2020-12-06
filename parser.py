dc6b3132c6826dc803cee240c1d4a738a3715e98
Lăscău Ionut Sebastian <32646125+lascau@users.noreply.github.com>
2020-12-06 23:02:15 +0200

    Add files via upload
    added drawing tree

--- parser.py
+++ parser.py
@@ -2,6 +2,12 @@ import pandas as pd
 import os
 import html # for unescape html
 import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.tree import Tree
+import itertools
+import time
+
 
 class Parser:
 
@@ -12,8 +18,32 @@ class Parser:
         data_answers = pd.read_csv(os.getcwd() + '/extracted_data/answers.csv')
         data_comments = pd.read_csv(os.getcwd() + '/extracted_data/comments.csv')
         data_questions = pd.read_csv(os.getcwd() + '/extracted_data/questions.csv')
-        tokens = [html.unescape(tok).split() for tok in data_answers['Answer'].head(10)]
-        print(tokens)
+        
+        tokenizer = nltk.RegexpTokenizer(r"\w+")
+        full_text = ''.join(data_answers['Answer'].head(2))
+        #print(full_text)
+        words = [word_tokenize(html.unescape(' '.join(tokenizer.tokenize(word)))) for word in data_answers['Answer'].head(10)]
+        #print(words)
+        '''
+        #it takes to much time when all answers are merged
+        words = list(itertools.chain.from_iterable(words))
+        '''
+        for word in words:
+            #start time
+            tic = time.perf_counter()
+
+            tagged = nltk.pos_tag(word)
+            #end time
+            toc = time.perf_counter()
+            print(f"Time : {(toc - tic)*60:0.4f} seconds")
+
+            # it takes much more time when we have a huge number of tagged words to deal with
+            pattern = "NP: {<DT>?<JJ>*<NN>}"
+            tagged = list(map(lambda sent: Tree(sent[1], children=[sent[0]]), tagged))
+            NPChunker = nltk.RegexpParser(pattern) 
+            result = NPChunker.parse(tagged)
+            result.draw()
+
         
     def export_to_csv(self):
         pass
