9411583c3e23f5c1b24b478c156d74264b7e87bf
Lăscău Ionut Sebastian <32646125+lascau@users.noreply.github.com>
2020-12-06 12:06:32 +0200

    Add files via upload
    refactored and improves the speed of plotting

--- graphplotter.py
+++ graphplotter.py
@@ -6,19 +6,40 @@ import datetime
 import calendar # for converting index month to name calendar.month_name[idx]
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from tkinter import BOTTOM
 import time
 import concurrent.futures
+from concurrent.futures import ThreadPoolExecutor
+
 
 class DataAnalyzor:
+
+    def convert(self, df, column_name):
+        return pd.to_datetime(df[column_name],
+                              format='%Y-%m-%d %H:%M:%S')
+
+    def read_answers(self):
+        self.df_answers   = pd.read_csv(os.getcwd() + '/extracted_data/answers.csv', usecols=['Answer', 'Creation Date'])
+
+    def read_questions(self):
+        self.df_questions = pd.read_csv(os.getcwd() + '/extracted_data/questions.csv', usecols=['Title', 'Content', 'Creation Date', 'Tags'])
+
+    def read_comments(self):
+        self.df_comments  = pd.read_csv(os.getcwd() + '/extracted_data/comments.csv', usecols=['Comment', 'Creation Date'])
     
     def __init__(self):
         start = time.time()
-        # data frames
-        self.df_answers = pd.read_csv(os.getcwd() + '/extracted_data/answers.csv')
-        self.df_questions = pd.read_csv(os.getcwd() + '/extracted_data/questions.csv')
-        self.df_comments = pd.read_csv(os.getcwd() + '/extracted_data/comments.csv')               
+             
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            executor.submit(self.read_answers)
+            executor.submit(self.read_questions)
+            executor.submit(self.read_comments)
+
+        self.df_answers['Creation Date'] = self.convert(self.df_answers, 'Creation Date')
+        self.df_comments['Creation Date'] = self.convert(self.df_comments, 'Creation Date')
+        self.df_questions['Creation Date'] = self.convert(self.df_questions, 'Creation Date')
+
         end = time.time()
+
         print('Time to load csvs', end-start)
 
     def draw_strategy(self, root, pos_x, pos_y, title, strategy):
@@ -28,13 +49,13 @@ class DataAnalyzor:
         bar.get_tk_widget().place(x = pos_x, y = pos_y)
         ax.set_title(title)
         strategy(ax)  
-       
+    
     def plot(self):
         start = time.time()
         root = tk.Tk()
+        root.state('zoomed')
         root.title('Stackoverflow trends analysis')
     
-         # info for doing concurency
         self.strategies = [
                             self.strategy_length_comments_per_day,
                             self.strategy_length_answers_per_day,
@@ -54,11 +75,11 @@ class DataAnalyzor:
                        'Number of words per days for answers',
                        'Number of words per days for answers']
         
-        #with concurent.futures.ProcessPoolExecutor() as executor:
+
         for pos_x, pos_y, title, strategy in zip(self.window_xposes,
-                                                     self.window_yposes,
-                                                     self.titles,
-                                                     self.strategies):
+                                                 self.window_yposes,
+                                                 self.titles,
+                                                 self.strategies):
             self.draw_strategy(root, pos_x, pos_y, title, strategy)
 
         
@@ -67,91 +88,82 @@ class DataAnalyzor:
         root.mainloop()
 
     def strategy_length_answers_per_day(self, ax):
-        #Answer, Creation Date
+
         data = {'Length of answer' : self.df_answers['Answer']
                                                          .map(lambda answer: len(answer))
                                                          .tolist(),
                 'Days'            : self.df_answers['Creation Date']
-                                                              .map(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").day)
+                                                              .map(lambda date: date.day)
                                                               .tolist()
                 }
         df = DataFrame(data, columns = ['Length of answer', 'Days'])
         df.plot(x = 'Days', y = 'Length of answer', kind = 'scatter', ax = ax)
-        #print(data['Length of answer'])
         plt.show()
 
     def strategy_length_comments_per_day(self, ax):
-        #Comment, Creation Date
+        
         data = {'Length of comment' : self.df_comments['Comment']
                                                          .map(lambda comment: len(comment))
                                                          .tolist(),
                 'Days'            : self.df_comments['Creation Date']
-                                                              .map(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").day)
+                                                              .map(lambda date: date.day)
                                                               .tolist()
                 }
         df = DataFrame(data, columns = ['Length of comment', 'Days'])
         df.plot(x = 'Days', y = 'Length of comment', color='y', kind = 'scatter', ax = ax)
-        #print(data['Length of answer'])
         plt.show()
 
     def strategy_length_questions_per_day(self, ax):
         
-        #Title Content, Creation Date, Tags
         data = {'Length of question' : self.df_questions['Title']
                                                          .map(lambda comment: len(comment))
                                                          .tolist(),
                 'Days'            : self.df_questions['Creation Date']
-                                                              .map(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").day)
+                                                              .map(lambda date: date.day)
                                                               .tolist()
                 }
         df = DataFrame(data, columns = ['Length of question', 'Days'])
         df.plot(x = 'Days', y = 'Length of question', color='r', kind = 'scatter', ax = ax)
-        #print(data['Length of answer'])
         plt.show()
 
     def strategy_words_per_day_in_comments(self, ax):
-        #Comment, Creation Date
+
         data = {'Number of words' : self.df_comments['Comment']
                                                          .map(lambda comment: len(comment.split(' ')))
                                                          .tolist(),
                 'Days'            : self.df_comments['Creation Date']
-                                                              .map(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").day)
+                                                              .map(lambda date: date.day)
                                                               .tolist()
                 }
         df = DataFrame(data, columns = ['Number of words', 'Days'])
         df.plot(x = 'Days', y = 'Number of words', kind = 'scatter', ax = ax)
-        #print(data['Length of answer'])
         plt.show()
     
     def strategy_words_per_day_in_answers(self, ax):
-        df_answers = pd.read_csv(os.getcwd() + '/extracted_data/answers.csv')
-        #Answer, Creation Date
+
         data = {'Number of words' : self.df_answers['Answer']
                                                          .map(lambda answer: len(answer.split(' ')))
                                                          .tolist(),
                 'Days'            : self.df_answers['Creation Date']
-                                                              .map(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").day)
+                                                              .map(lambda date: date.day)
                                                               .tolist()
                 }
         df = DataFrame(data, columns = ['Number of words', 'Days'])
         df.plot(x = 'Days', y = 'Number of words', kind = 'scatter', ax = ax)
-        #print(data['Length of answer'])
         plt.show()
         
     
     def strategy_words_per_day_in_questions(self, ax):
 
-        #Title Content, Creation Date, Tags
         data = {'Number of words' : self.df_questions['Title']
                                                          .map(lambda comment: len(comment.split(' ')))
                                                          .tolist(),
                 'Days'            : self.df_questions['Creation Date']
-                                                              .map(lambda date: datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S").day)
+                                                              .map(lambda date: date.day)
                                                               .tolist()
                 }
         df = DataFrame(data, columns = ['Number of words', 'Days'])
         df.plot(x = 'Days', y = 'Number of words', kind = 'scatter', ax = ax)
-        #print(data['Length of answer'])
         plt.show()
 
     
